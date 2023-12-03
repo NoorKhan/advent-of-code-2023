b50099a235ba93f1ca7000b7f6e5733b1f827ec6
Noor Khan <noordotkhan@gmail.com>
2023-12-03 14:55:19 -0500

    minor

--- 3/3.lisp
+++ 3/3.lisp
@@ -1,4 +1,10 @@
 (defun initialize-engine-schematic (input-file)
-  )
+  (with-open-file (stream input-file)
+      (do ((line (read-line stream nil)
+		 (read-line stream nil)))
+	  ((null line))
+	(print line))))
+
+(initialize-engine-schematic "test input.txt")
 
 
