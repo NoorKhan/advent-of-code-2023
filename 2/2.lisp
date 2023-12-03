fd4b4018ce6e5320498c29bcf13cf56dd9089f2f
Noor Khan <noordotkhan@gmail.com>
2023-12-03 14:11:42 -0500

    refactored

--- 2/2.lisp
+++ 2/2.lisp
@@ -6,30 +6,25 @@
 (defun get-valid-game-ids (input-file bag)
   (let ((valid-game-ids '()))
     (with-open-file (stream input-file)
-     (do ((line (read-line stream nil)
-		(read-line stream nil)))
-	 ((null line))
-       (let* ((colon-index (search ":" line))
-	      (game-id (parse-integer (subseq line (+ 1 (search " " line)) colon-index)))
-	      (subsets (uiop:split-string (subseq line (+ 1 colon-index)) :separator ";"))
-	      (valid t))
-	 (loop for subset in subsets
-	    while valid
-	    do (setf valid (is-valid-subset subset bag)))
-	 (if valid (setf valid-game-ids (append (list game-id) valid-game-ids))))))
+      (do ((line (read-line stream nil)
+		 (read-line stream nil)))
+	  ((null line))
+	(let* ((colon-index (search ":" line))
+	       (subsets (uiop:split-string (subseq line (+ 1 colon-index)) :separator ";"))
+	       (game-id (parse-integer (subseq line (+ 1 (search " " line)) colon-index))))
+	  (when (loop for subset in subsets
+		   always (valid-subset-p subset bag))
+	    (setf valid-game-ids (append (list game-id) valid-game-ids))))))
     valid-game-ids))
 
-(defun is-valid-subset (subset bag)
-  (let ((cubes (uiop:split-string subset :separator ","))
-	(valid t))
+(defun valid-subset-p (subset bag)
+  (let ((cubes (uiop:split-string subset :separator ",")))
     (loop for cube in cubes
-       while valid
-       do (let* ((cube-info (uiop:split-string (string-trim '(#\Space #\Tab #\Newline #\Return) cube)))
-		 (cube-count (parse-integer (car cube-info)))
-		 (cube-color (cadr cube-info))
-		 (count-in-bag (gethash cube-color bag)))
-	    (setf valid (and count-in-bag (>= count-in-bag cube-count)))))
-    valid))
+       for cube-info = (uiop:split-string (string-trim '(#\Space #\Tab #\Newline #\Return) cube))
+       for cube-count = (parse-integer (car cube-info))
+       for cube-color = (cadr cube-info)
+       for count-in-bag = (gethash cube-color bag)
+       always (and count-in-bag (>= count-in-bag cube-count)))))
 
 ;; part 1
 (reduce #'+ (get-valid-game-ids "input.txt" *bag*))
@@ -44,21 +39,22 @@
 	       (subsets (uiop:split-string (subseq line (+ 1 colon-index)) :separator ";"))
 	       (min-bag (make-hash-table :test 'equal)))
 	  (loop for subset in subsets
-	     do (let ((cubes (uiop:split-string subset :separator ",")))
-		  (loop for cube in cubes
-		     do (update-min-bag subset min-bag))))
-	  (setf game-powers (append (list (reduce #'* (loop for value being the hash-values of min-bag
-							 collect value))) game-powers)))))
+	     do (update-min-bag subset min-bag))
+	  (setf game-powers (append (list (get-game-power min-bag)) game-powers)))))
     game-powers))
+
+(defun get-game-power (min-bag)
+  (reduce #'* (loop for value being the hash-values of min-bag collect value)))
 
 (defun update-min-bag (subset min-bag)
   (let ((cubes (uiop:split-string subset :separator ",")))
     (loop for cube in cubes
-       do (let* ((cube-info (uiop:split-string (string-trim '(#\Space #\Tab #\Newline #\Return) cube)))
-		 (cube-count (parse-integer (car cube-info)))
-		 (cube-color (cadr cube-info))
-		 (count-in-bag (gethash cube-color min-bag)))
-	    (if (or (null count-in-bag) (< count-in-bag cube-count)) (setf (gethash cube-color min-bag) cube-count))))
+       for cube-info = (uiop:split-string (string-trim '(#\Space #\Tab #\Newline #\Return) cube))
+       for cube-count = (parse-integer (car cube-info))
+       for cube-color = (cadr cube-info)
+       for count-in-bag = (gethash cube-color min-bag)
+       do (when (or (null count-in-bag) (< count-in-bag cube-count))
+	    (setf (gethash cube-color min-bag) cube-count)))
     min-bag))
 
 ;; part 2
