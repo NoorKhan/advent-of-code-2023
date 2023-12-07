e5251c7a94ce46210ec6b081652397e471746f8e
Noor Khan <noordotkhan@gmail.com>
2023-12-07 15:49:58 -0500

    day 4 part 2 work

--- 4/4.lisp
+++ 4/4.lisp
@@ -9,7 +9,7 @@
 	  ((null line))
 	(let* ((trimmed-line (string-trim '(#\Space #\Tab #\Newline #\Return) line))
 	       (split-line (uiop:split-string trimmed-line :separator ":"))
-	       (card-id (cadr (uiop:split-string (car split-line))))
+	       (card-id (parse-integer (cadr (uiop:split-string (car split-line)))))
 	       (numbers (uiop:split-string (cadr split-line) :separator "|"))
 	       (winning-numbers (parse-numbers (car numbers)))
 	       (your-numbers (parse-numbers (cadr numbers)))
@@ -17,21 +17,46 @@
 	  (print winning-numbers)
 	  (print your-numbers)
 	  (format t "card-id: ~a winnings numbers: ~a your numbers: ~a~%" card-id winning-numbers your-numbers)
-	  (format t "matches in card id: ~a: ~a~%" card-id matches)
+	  (format t "matches in card id ~a: ~a~%" card-id matches)
 	  (when (> matches 0) (setf points (+ points (expt 2 (- matches 1))))))))
     points))
 
 (get-points "test input.txt")
 (get-points "input.txt")
-(fset:size (fset:intersection (fset:set 1 2 3) (fset:set 1 2 4)))
 
-(fset:contains? (fset:set 1 2 3) 7)
+(defun get-total-cards (input-file)
+  (let ((total-cards 0)
+	(cards-to-check '())
+	(card-map (make-hash-table)))
+    (with-open-file (stream input-file)
+      (do ((line (read-line stream nil)
+		 (read-line stream nil)))
+	  ((null line))
+	(let* ((trimmed-line (string-trim '(#\Space #\Tab #\Newline #\Return) line))
+	       (split-line (uiop:split-string trimmed-line :separator ":"))
+	       (card-id (parse-integer (car (last (uiop:split-string
+					      (string-trim '(#\Space #\Tab #\Newline #\Return)
+							   (car split-line)))))))
+	       (numbers (uiop:split-string (cadr split-line) :separator "|"))
+	       (winning-numbers (parse-numbers (car numbers)))
+	       (your-numbers (parse-numbers (cadr numbers)))
+	       (matches (fset:size (fset:intersection winning-numbers your-numbers))))
+	  (setf (gethash card-id card-map) (cons winning-numbers your-numbers))
+	  (setf cards-to-check (append cards-to-check (list card-id))))))
+    (print cards-to-check)
+    (loop while (> (length cards-to-check) 0) do
+      (let* ((card-id (car cards-to-check))
+	     (numbers (gethash card-id card-map))
+	     (matches (fset:size (fset:intersection (car numbers) (cdr numbers)))))
+	(incf total-cards)
+	(when (> matches 0)
+	  (loop for i from 1 to matches do
+	    (setf cards-to-check (append cards-to-check (list (+ card-id i))))))
+	(setf cards-to-check (cdr cards-to-check))))
+    total-cards))
 
-(fset:union (fset:set '(1 2 3) '(1 2 3) '(1 2 4)) (fset:set '(1 2 3) '(1 2 3) '(1 8 4)))
-(fset:set-fset '(1 2 3))
-
-(expt 2 3)
-(to-set '(1 3 2 3))
+(print (get-total-cards "test input.txt"))
+(print (get-total-cards "input.txt"))
 
 (defun parse-numbers (numbers)
   (to-set (map 'list
@@ -43,8 +68,3 @@
 (defun to-set (list)
   (reduce (lambda (x y) (fset:union x (fset:set y))) list :initial-value (fset:empty-set)))
 
-(search ":" "Card 1:")
-(cadr (uiop:split-string "A:B" :separator ":"))
-(subseq "abc" 1)
-
-(remove-if #'(lambda (x) (parse-integer x)) '("1" "a" "2"))
