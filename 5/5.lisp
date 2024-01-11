f1a3e0f8b67279ccb1b19edc52035cdc10f6a733
Noor Khan <noordotkhan@gmail.com>
2024-01-11 16:18:26 -0500

    test case working, but need to change up the logic

--- 5/5.lisp
+++ 5/5.lisp
@@ -10,6 +10,9 @@
 (defparameter *temperature-to-humidity-key* "temperature-to-humidity")
 (defparameter *humidity-to-location-key* "humidity-to-location")
 
+(defparameter *keys* (list *seeds-key* *seed-to-soil-key* *soil-to-fertilizer-key* *fertilizer-to-water-key*
+		       *water-to-light-key* *light-to-temperature-key* *temperature-to-humidity-key* *humidity-to-location-key*))
+
 (defun parse-input (input-file-name)
   (let ((parsed-input-map (make-hash-table :test #'equal))
 	(current-key ""))
@@ -50,19 +53,31 @@
 				  (+ destination-range-start i)))))))))
     parsed-input-map))
 
+(defun get-min-location (parsed-input-map)
+  (let ((seeds (gethash *seeds-key* parsed-input-map))
+	(min-location nil))
+    (loop for seed in seeds
+	  do (let* ((soil (get-value (gethash *seed-to-soil-key* parsed-input-map) seed))
+			 (fertilizer (get-value (gethash *soil-to-fertilizer-key* parsed-input-map) soil))
+			 (water (get-value (gethash *fertilizer-to-water-key* parsed-input-map) fertilizer))
+			 (light (get-value (gethash *water-to-light-key* parsed-input-map) water))
+			 (temperature (get-value (gethash *light-to-temperature-key* parsed-input-map) light))
+			 (humidity (get-value (gethash *temperature-to-humidity-key* parsed-input-map) temperature))
+			 (location (get-value (gethash *humidity-to-location-key* parsed-input-map) humidity)))
+		    (when (or (null min-location) (> min-location location))
+		      (setf min-location location))))
+    min-location))
+
+(defun get-value (source-to-destination-map source)
+  (let ((value (gethash source source-to-destination-map)))
+    (if value value source)))
+
+(get-min-location (parse-input "input.txt"))
+
 (defun trim-string-whitespace (string)
   (string-trim '(#\Space #\Tab #\Newline #\Return) string))
 
 (defun parse-to-integers (string-numbers)
   (mapcar #'(lambda (s) (parse-integer s))
 	  (uiop:split-string (trim-string-whitespace string-numbers) :separator " ")))
-
-(gethash *seed-to-soil-key* (parse-input "test input.txt"))
-(gethash *seeds-key* (parse-input "test input.txt"))
-
-(parse-to-integers "1 2 44 3")
-(uiop:split-string " 1 2          3" :separator " ")
-
-(-<> 1 (+ <> <>) (-<> (+ <> <> <>)))
-
 
